6401371ed1e0249fddea92dc3faac80abf7730b7
veyselkaya <veyselkaya03@gmail.com>
2023-07-13 14:54:29 +0300

    add test fontions

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class HomePagetests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self): # new
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_template_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, '<h1>Homepage</h1>')
+
+
+class AboutPagetests(SimpleTestCase):
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):  # new
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('about'))
+        self.assertContains(response, '<h1>About Page</h1>')
+
+
+
+# terminalden python manage.py test yazarak deneyebilirsiniz.
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 
 # Create your views here.
 
 
-def homPageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    # return HttpResponse('Hello, World!')
+    template_name = "home.html"
+    
+
+
+class AboutPageView(TemplateView): # new
+    template_name = "about.html"
